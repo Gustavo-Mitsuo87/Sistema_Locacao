eff1a55ff95698677aca57d68ecb5cd2e7c7470f
GuilhermeO-Gomes <guilherme16062009@gmail.com>
2026-09-06 22:12:57 -0300

    Deixando umas coisas prontas

---
+++ src/util/Conexao.java
@@ -0,0 +1,7 @@
+package util;
+
+public final class Conexao {
+	private static final String URL = "jdbc:mysql://localhost:3306/locacao_carro";
+	private static final String USUARIO	= "root";
+	private static final String SENHA	= "";
+}
