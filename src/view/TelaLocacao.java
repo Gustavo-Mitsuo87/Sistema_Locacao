6dd49ad250157ba0c2cb97147836e8ae0ffbfbf8
Gustavo-Mitsuo87 <gustavomitsuo1@gmail.com>
2026-09-10 00:17:20 -0300

    feat: Tela de locação feita

--- src/view/TelaLocacao.java
+++ src/view/TelaLocacao.java
@@ -1,7 +1,144 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.*;
 
-public class TelaLocacao {
+public class TelaLocacao extends JPanel{
 
+	private static final long serialVersionUID = 1L;
+	
+	private final JLabel lblNome = new JLabel("Nome: "),
+			lblCPF = new JLabel("CPF: "),
+			lblCNH = new JLabel("CNH: "),
+			lblVeiculo = new JLabel("Veículo: "),
+			lblPlaca = new JLabel("Placa: "),
+			lblCategoria = new JLabel("Categoria: "),
+			lblDiaria = new JLabel("Valor Diária: R$"),
+			lblRetirada = new JLabel("Retirada: "),
+			lblDevolucao = new JLabel("Devolução: "),
+			lblTotal = new JLabel("Valor Total: R$");
+
+	private final JTextField txtNome = new JTextField(),
+			txtCPF = new JTextField(),
+			txtCNH = new JTextField(),
+			txtVeiculo = new JTextField(),
+			txtPlaca = new JTextField(),
+			txtCategoria = new JTextField(),
+			txtDiaria = new JTextField(),
+			txtRetirada = new JTextField(),
+			txtDevolucao = new JTextField(),
+			txtTotal = new JTextField();
+			
+	
+	public TelaLocacao() {
+		montar();
+	}
+	
+	private void montar() {
+		
+		txtNome.setEditable(false);          
+	    txtNome.setBorder(null);             
+	    txtNome.setOpaque(false);            
+	    txtNome.setFocusable(false); 
+	    
+		txtCPF.setEditable(false);          
+	    txtCPF.setBorder(null);             
+	    txtCPF.setOpaque(false);            
+	    txtCPF.setFocusable(false); 
+	    
+		txtCNH.setEditable(false);          
+	    txtCNH.setBorder(null);             
+	    txtCNH.setOpaque(false);            
+	    txtCNH.setFocusable(false); 
+	    
+	    txtVeiculo.setEditable(false);          
+	    txtVeiculo.setBorder(null);             
+	    txtVeiculo.setOpaque(false);            
+	    txtVeiculo.setFocusable(false); 
+	    
+	    txtPlaca.setEditable(false);          
+	    txtPlaca.setBorder(null);             
+	    txtPlaca.setOpaque(false);            
+	    txtPlaca.setFocusable(false); 
+	    
+	    txtCategoria.setEditable(false);          
+	    txtCategoria.setBorder(null);             
+	    txtCategoria.setOpaque(false);            
+	    txtCategoria.setFocusable(false); 
+	    
+	    txtRetirada.setEditable(false);          
+	    txtRetirada.setBorder(null);             
+	    txtRetirada.setOpaque(false);            
+	    txtRetirada.setFocusable(false); 
+	    
+	    txtDevolucao.setEditable(false);          
+	    txtDevolucao.setBorder(null);             
+	    txtDevolucao.setOpaque(false);            
+	    txtDevolucao.setFocusable(false);
+	    
+	    txtDiaria.setEditable(false);          
+	    txtDiaria.setBorder(null);             
+	    txtDiaria.setOpaque(false);            
+	    txtDiaria.setFocusable(false);
+	    
+	    txtTotal.setEditable(false);          
+	    txtTotal.setBorder(null);             
+	    txtTotal.setOpaque(false);            
+	    txtTotal.setFocusable(false); 
+	    
+	    
+	    setLayout(new BorderLayout());
+	    
+	    JPanel centro = new JPanel();
+	    centro.setLayout(new BoxLayout(centro, BoxLayout.Y_AXIS));
+
+		
+		JPanel cliente = new JPanel(new GridLayout(3, 2, 10, 10));
+		cliente.setBorder(BorderFactory.createTitledBorder("Dados do Cliente"));
+		cliente.add(lblNome, txtNome);
+		cliente.add(lblCPF, txtCPF);
+		cliente.add(lblCNH, txtCNH);
+		
+		
+		JPanel veiculo = new JPanel(new GridLayout(4, 2, 10, 10));
+		veiculo.setBorder(BorderFactory.createTitledBorder("Dados do Veículo"));
+		veiculo.add(lblVeiculo, txtVeiculo);
+		veiculo.add(lblPlaca, txtPlaca);
+		veiculo.add(lblCategoria, txtCategoria);
+		veiculo.add(lblDiaria, txtDiaria);
+		
+		
+		JPanel locacao = new JPanel(new GridLayout(3, 2, 10, 10));
+		locacao.setBorder(BorderFactory.createTitledBorder("Dados da Locação"));
+		locacao.add(lblRetirada, txtRetirada);
+		locacao.add(lblDevolucao, txtDevolucao);
+		locacao.add(lblTotal, txtTotal);
+		
+		JPanel painelBotao = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		JButton confirma = new JButton("Confirmar Locação");
+		
+		painelBotao.add(confirma);
+		
+		centro.add(cliente);
+		centro.add(veiculo);
+		centro.add(locacao);
+		
+		centro.add(painelBotao);
+		add(centro, BorderLayout.CENTER);
+		
+		
+		confirma.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		
+		
+	}
 }
--- src/view/TelaTeste.java
+++ src/view/TelaTeste.java
@@ -14,7 +14,7 @@ public class TelaTeste extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		
-		add(new TelaResumoLocacao());
+		add(new TelaLocacao());
 		setVisible(true);
 	}
 	
